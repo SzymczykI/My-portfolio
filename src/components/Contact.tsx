f96153f0798d4badc3a109d8eff195f08094c15b
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-05-19 23:55:21 +0200

    error with icon fixed

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,4 @@
 import style from "../../styles/Contact.module.css";
-import github from "../assets/GitHub.png";
-import linkedin from "../assets/linkedin.png";
 import Image from "next/image";
 
 const Contact = () => {
@@ -11,10 +9,10 @@ const Contact = () => {
       </div>
       <div className={style.links_box}>
         <a className={style.logo} href="https://github.com/SzymczykI">
-          <Image width={40} height={40} src={github} alt="github" />
+          <Image width={40} height={40} src="/GitHub.png" alt="github" />
         </a>
         <a className={style.logo} href="https://www.linkedin.com/in/izabela-szymczyk-603514114/">
-          <Image  width={40} height={40} src={linkedin} alt="linkedin" />
+          <Image  width={40} height={40} src="/linkedin.png" alt="linkedin" />
         </a>
       </div>
     </section>
