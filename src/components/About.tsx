96e5d0454b9f35c53558ba05783f76cc34224955
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-05-30 11:20:26 +0200

    new photo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,18 +5,29 @@ const About = () => {
   return (
     <section className={style.container}>
       <div className={style.about_box}>
+        <div>
+          <Image
+            src="/izabela2.jpg"
+            alt="Picture of the author"
+            width={450}
+            height={550}
+          />
+        </div>
         <div className={style.text_box}>
           <h2 className={style.title}>About me</h2>
           <p className={style.description}>
             {" "}
             Hi, I'm Izabela! <br></br>
-            Open-minded and creative Full Stack JavaSript Developer living in
-            Oslo, Norway. I am Finance and Accoutancy graduated, experienced in e-commerce and
-            sales in the insurance, clothing, cosmetics industries and also in
-            creating and running my own business. Finally, I'm making my biggest
-            dream come true by focusing entirely on programming.{" "}
+            Open-minded and creative Full Stack JavaScript Developer from Poland
+            living in Oslo, Norway. I am a Finance and Accountancy graduate,
+            experienced in  e-commerce and sales in the insurance, clothing,
+            cosmetics industries and also in creating and running my own
+            business. Finally, I'm making my biggest dream come true by focusing
+            entirely on my passion - programming{" "}
           </p>
-          <h3 className={style.subtitle}>School of applied technology - SALT</h3>
+          <h3 className={style.subtitle}>
+            School of applied technology - SALT
+          </h3>
           <p className={style.description}>
             From over 1000 applicants, I was one of ~30 students selected to
             partake in a highly paced and very intensive Full Stack JavaScript
@@ -27,19 +38,11 @@ const About = () => {
           </p>
           <h3 className={style.subtitle}>Tech Skills</h3>
           <p className={style.description}>
-            JavaScript, React, Node.js, Next.js, Express, HTML5, CSS3, Databases
-            incl. MongoDB and PostgreSQL, UX/UI and general development tools
-            such as Git, REST, Docker, Kubernetes, Netlify, Redux, GraphQL, AWS,
-            TDD and CI/CD.
+            JavaScript, TypeScript, React, Node.js, Next.js, Express, HTML5,
+            CSS3, Databases incl. MongoDB and PostgreSQL, UX/UI and general
+            development tools such as Git, REST, Docker, Kubernetes, Netlify,
+            Redux, GraphQL, AWS, TDD and CI/CD.
           </p>
-        </div>
-        <div>
-          <Image
-            src="/izabela.png"
-            alt="Picture of the author"
-            width={500}
-            height={550}
-          />
         </div>
       </div>
     </section>
